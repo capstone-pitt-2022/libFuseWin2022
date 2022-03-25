7765b8fdc405df80639554ef527911f383b17b14
qzw68 <qiw68@pitt.edu>
2022-03-24 20:01:37 -0400

    mkdir rmdir test init

--- test/ntapfuse_test.py
+++ test/ntapfuse_test.py
@@ -1,9 +1,12 @@
 # ntapfuse unit testing version 0.1
 # code by @author Carter S. Levinson <carter.levinson@pitt.edu>
+from operator import sub
 import unittest
 import subprocess
 import sqlite3
 import regex
+import pytest
+import os
 
 # Note: that there is shell builtin command to interface with every syscall
 
@@ -21,14 +24,120 @@ import regex
 # TODO: think about how the OS and the subprocess layer affect our testing
 # environment and procedure
 
+mountName = "mountPoint"
+baseDir = "testbase"
+dbName = "log.db"
+blockSize = 4096
+usage=0
+numLogs = 0
+
+# need to do more thinking about how this will work
+def setup_test_env():
+    # con = sqlite3.connect('log.db')
+    # cur = con.cursor()
+    print("creating basedir and mountpoint...")
+    os.chdir("..")
+    cmd = '''
+    rmdir %s/*
+    rmdir %s
+    sudo make install
+    sudo umount %s 
+    rmdir %s
+    mkdir %s   
+    mkdir %s
+    rm %s
+    '''%(baseDir,baseDir,mountName,mountName,mountName,baseDir,dbName)
+
+    os.system(cmd)
+    res=subprocess.run("ntapfuse mount %s %s"%(baseDir,mountName),shell=True,check=True,capture_output=True,text=True)
+    if res.stderr:
+        print("stderr:",res.stderr)
+
+
+
+
+setup_test_env()
+
+
 
 class TestClass:
 
+   
     def test_mkdir(self):
-        return
 
+        global usage
+        global numLogs
+
+        print("Testing mkdir...")
+
+        os.chdir("./%s"%mountName)
+
+        # create some empty folders
+      
+        for i in range(30):
+            res = subprocess.run('mkdir testFolder%s'%str(i),shell=True,check=True,capture_output=True,text=True)
+            
+            if res.stderr:
+                print("stderr:",res.stderr)
+            else:
+                usage+=blockSize
+                numLogs+=1
+                
+
+        # check if usage and logs match
+        os.chdir("..")  
+        uid=os.getuid()
+        usageRes = check_quota_db(self,uid)
+        numLogsRes = check_log_db(self,uid,None)
+
+        print("expecting numbers of logs: %s  result is: %s"%(str(numLogs),str(numLogs)))
+        print("expecting user usage: %s  result is: %s"%(str(usage),str(usageRes)))
+
+
+        assert usageRes==usage and numLogsRes==numLogs
+        
+
+     
     def test_rmdir(self):
-        return
+        # need to assume mkdir work
+        print("Testing rmdir...")
+        global usage
+        global numLogs
+
+        os.chdir("./%s"%mountName)
+
+        # delete some empty folders that mkdir create
+        for i in range(10):
+            res = subprocess.run('rmdir testFolder%s'%str(i),shell=True,check=True,capture_output=True,text=True)
+            if res.stderr:
+                print("stderr:",res.stderr)
+            else:
+                usage-=blockSize
+                numLogs+=1
+
+        os.chdir("..")
+
+        # mkdir rmdir test done, remove test folders
+        cmd = '''
+            rmdir %s/*
+            rmdir %s
+            sudo umount %s
+            rm %s/* -f
+            rmdir %s
+            '''%(baseDir,baseDir,mountName,mountName,mountName)
+
+        # check if usage and logs match
+        os.system(cmd)
+        uid=os.getuid()
+        usageRes = check_quota_db(self,uid)
+        numLogsRes = check_log_db(self,uid)
+
+        print("expecting numbers of logs: %s  result is: %s"%(str(numLogs),str(numLogs)))
+        print("expecting user usage: %s  result is: %s"%(str(usage),str(usageRes)))
+
+        assert usageRes==usage and numLogsRes==numLogs
+
+    
 
     def test_write(self):
         return
@@ -48,18 +157,38 @@ class TestClass:
     def test_utime(self):
         return 
 
-def check_log_db(self):
-    return
-
-def check_quota_db(self):
-    return
-
-
-# need to do more thinking about how this will work
-def setup_test_env():
-    con = sqlite3.connect('log.db')
+def check_log_db(self,uid,op=None):
+    con = sqlite3.connect(dbName)
+    uid=os.getuid()
     cur = con.cursor()
+    if op:
+        cur.execute("select count(*) from Logs where UID=%s and Operation=%s"%(uid,op))
+    else:
+        cur.execute("select count(*) from Logs where UID=%s"%uid)
+    res = cur.fetchall()
+    con.close()
+    return res[0][0]
+    
+
+def check_quota_db(self,uid):
+    con = sqlite3.connect(dbName)
+    uid=os.getuid()
+    cur = con.cursor()
+    cur.execute("select Usage from Quotas where UID=%s"%uid)
+    res = cur.fetchall()
+    con.close()
+    return res[0][0]  # return the usage of an user if valid
+
+
+
+
+
+
+
+
+
 
 
 def destroy_test_env():
-    con.close()
+    # con.close()
+    return
